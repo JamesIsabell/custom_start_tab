e7d1495501b5ff73e83abe1e626b39a903aa74fd
James Isabell <j9isabell@gmail.com>
2025-01-08 14:58:20 -0600

    Update script.js

--- script.js
+++ script.js
@@ -4,110 +4,57 @@
 const MASTER_MAP = [
     
     {
-        "groupName": "Fulfillment",
+        "groupName": "Brand",
         "subGroups": [
             {
-                "groupName": "General",
+                "groupName": "",
                 "items":[
-                    {"name": "Fresh Desk", "url": "https://nile.freshdesk.com/a/tickets/filters/unresolved"},
                     {"name": "Shopify", "url": "https://partners.shopify.com/801562/stores"},
-                    {"name": "TMS", "url": "https://skandh-tms-tugg.eastus.cloudapp.azure.com/home"},
-                    {"name": "Fulfillment Dashboard", "url": "https://app.powerbi.com/groups/me/apps/998bbb17-e40e-4b2b-afd7-e417b59ccc99/reports/f5884623-8d69-49c8-83e0-3299e1b1f0d3/11fec9da8cf04e6a5d94?experience=power-bi"},
+                    {"name": "Infor Desktop", "url": "https://mingle-portal.inforcloudsuite.com/v2/X9K98RYL2D6P5AD4_PRD/87a130c6-35e8-4961-8aa0-3f4891ae549a"},
+                    {"name": "Media Requests", "url": "https://form.asana.com/?k=1E8U4gDZmzJ2D0bHzptrug&d=1155700125982772"},
+                    {"name": "Amazon", "url": "https://www.amazon.com/"},
+                    {"name": "Amazon SC", "url": "https://sellercentral.amazon.com/gp/homepage.html/ref=xx_home_logo_xx"},
+                    {"name": "OEC Group", "url": "https://portal.oecgroup.com/dashboard"},
+                    {"name": "Gorgias", "url": "https://barraairguns.gorgias.com/app/views"},
                 ]
             },
-            {
-                "groupName": "Infor",
-                "items":[
-                    {"name": "Desktop", "url": "https://mingle-portal.inforcloudsuite.com/v2/X9K98RYL2D6P5AD4_PRD/87a130c6-35e8-4961-8aa0-3f4891ae549a"},
-                    {"name": "Mobile", "url": "https://prd.sce.inforcloudsuite.com/scp11/sce/mobile-web-client/inforMetaClient.html?tenant=X9K98RYL2D6P5AD4_PRD"},
-                    {"name": "Training", "url": "https://mingle-portal.inforcloudsuite.com/X9K98RYL2D6P5AD4_TRN"},
-                    {"name": "Testing", "url": "https://mingle-portal.inforcloudsuite.com/X9K98RYL2D6P5AD4_TST"},
-                    {"name": "Documentation", "url": "https://docs.infor.com/wms/2022.x/en-us/useradminlib/default.html"},
-                    {"name": "Infor U", "url": "https://campus.infor.com/"},
-                    {"name": "Customer Portal", "url": "https://customerportal.infor.com/csmcore"},
-                ]
-            }
         ]
     },
     {
         "groupName": "Systems",
         "subGroups": [
             {
-                "groupName": "NetSuite",
+                "groupName": "",
                 "items":[
                     {"name": "Terra Rose", "url": "https://6751627.app.netsuite.com/app/center/card.nl?sc=-29&whence="},
-                    {"name": "Great Star", "url": "https://6145117.app.netsuite.com/app/center/card.nl?sc=-29&whence="},
-                    {"name": "Everyone Designs", "url": "https://7236299.app.netsuite.com/app/center/card.nl?sc=-29&whence="},
-                    {"name": "House of Outdoors", "url": "https://7576050.app.netsuite.com/app/center/card.nl?sc=-29&whence="},
-                    {"name": "Efofex", "url": "https://8157261.app.netsuite.com/app/center/card.nl?sc=-29&whence="}
-                ]
-            },
-            {
-                "groupName": "Microsoft",
-                "items":[
                     {"name": "Microsoft 365", "url": "https://www.microsoft365.com/?auth=2"},
                     {"name": "OneDrive", "url": "https://terrarose-my.sharepoint.com/personal/james_choosenile_com//_layouts/15/onedrive.aspx?login_hint=james%40choosenile%2Ecom&view=1"},
                     {"name": "Azure", "url": "https://portal.azure.com/#home"},
-                    {"name": "Admin", "url": "https://admin.microsoft.com/?auth_upn=james%40choosenile.com&source=applauncher#/homepage"}
+                    {"name": "Admin", "url": "https://admin.microsoft.com/?auth_upn=james%40choosenile.com&source=applauncher#/homepage"},
+                    {"name": "Retail Link", "url": "https://retaillink.login.wal-mart.com/login"},
+                    {"name": "Link Vault", "url": "https://app.merchantlabs.io/"},
+                    
                 ]
             },
-        ]
-    },
-    {
-        "groupName": "Integrations",
-        "subGroups": [
-            {
-                "groupName": "Switchboard",
-                "items":[
-                    {"name": "Deployment", "url": "https://merchant-labs.astronomer.run/dm0mate0/home"},
-                    {"name": "Local Instance", "url": "http://localhost:8081/"},
-                    {"name": "Cosmos", "url": "https://portal.azure.com/#@terrarose.com/resource/subscriptions/6d4d748d-18c9-4126-a3b6-9c5cf87fb739/resourceGroups/Switchboard/providers/Microsoft.DocumentDb/databaseAccounts/nile-switchboard/overview"},
-                    {"name": "Blob Storage", "url": "https://portal.azure.com/#@terrarose.com/resource/subscriptions/6d4d748d-18c9-4126-a3b6-9c5cf87fb739/resourceGroups/Switchboard/providers/Microsoft.Storage/storageAccounts/switchboard/overview"},
-                    {"name": "sFTP Storage", "url": "https://portal.azure.com/#@terrarose.com/resource/subscriptions/6d4d748d-18c9-4126-a3b6-9c5cf87fb739/resourcegroups/Switchboard/providers/Microsoft.Storage/storageAccounts/switchboardsftp/overview"}, 
-                    {"name": "True Commerce", "url": "https://foundry.truecommerce.com/core/Default.html"}
-                ]
-            },
-            {
-                "groupName": "Soapbox",
-                "items":[
-                    {"name": "Deployment", "url": "https://merchant-labs.astronomer.run/dzor8z0d/home"},
-                    {"name": "Local Instance", "url": "http://localhost:8080/"},
-                    {"name": "Cosmos", "url": "https://portal.azure.com/#@terrarose.com/resource/subscriptions/6d4d748d-18c9-4126-a3b6-9c5cf87fb739/resourceGroups/mlabs/providers/Microsoft.DocumentDb/databaseAccounts/mlabs/overview"},
-                    {"name": "Vercel", "url": "https://vercel.com/mlabs/linker"},
-                    {"name": "Blob Storage", "url": "https://portal.azure.com/#@terrarose.com/resource/subscriptions/6d4d748d-18c9-4126-a3b6-9c5cf87fb739/resourceGroups/mlabs/providers/Microsoft.Storage/storageAccounts/mlabstorage/overview"},
-                    {"name": "Reports", "url": "https://terrarose.sharepoint.com/sites/nile/Shared%20Documents/Forms/AllItems.aspx?id=%2Fsites%2Fnile%2FShared%20Documents%2FReports&viewid=42a6f1a6%2D6881%2D4632%2D9d5c%2D07b55a3a8a8d"}
-                ]
-            }
-
         ]
     },
     {
         "groupName": "Tools",
         "subGroups": [
             {
-                "groupName": "Work",
+                "groupName": "",
                 "items":[
                     {"name": "GitHub", "url": "https://github.com/orgs/nilecommerce/repositories"},
-                    {"name": "Link Vault", "url": "https://app.merchantlabs.io/"},
                     {"name": "Bill Dashboard", "url": "https://app.divvy.co/companies/Q29tcGFueTozNzM3NA==/home/dashboard?timePeriod=last_12_months"},
                     {"name": "Rippling", "url": "https://app.rippling.com/dashboard"},
                     {"name": "UniFy Portal", "url": "https://unifi.ui.com/consoles"},
-                    {"name": "Amazon SC", "url": "https://sellercentral.amazon.com/gp/homepage.html/ref=xx_home_logo_xx"},
                     {"name": "Brightwheel", "url": "https://schools.mybrightwheel.com/org"},
-                    {"name": "LastPass", "url": "https://lastpass.com/company/#!/users/"},
                     {"name": "Adobe Admin", "url": "https://adminconsole.adobe.com/2A7B4586619E9ADB0A495F85@AdobeOrg/overview"},
                     {"name": "Verkada", "url": "https://command.verkada.com/devices"},
                     
                     
                 ]
-            },
-            {
-                "groupName": "Personal",
-                "items":[
-                    {"name": "Gmail", "url": "https://mail.google.com/mail/u/0/#inbox"},
-                    {"name": "GitHub", "url": "https://github.com/JamesIsabell?tab=repositories"},
-                ]
-            },
+            }
         ]
     }
 ]
